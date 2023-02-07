3d0f77886afa4789463523772716d59bbe198264
raman41k <romangugel@gmail.com>
2023-02-07 15:23:55 +0200

    create header

---
+++ js/index.js
@@ -0,0 +1,30 @@
+const products = [
+    {
+        "id": 1,
+        "logo": "https://cdn.albero.com.ua/products/225472ae/b33c/11e7/8931/000c2987a3fe/100x100/3dd06-askania-grande.webp",
+        "title": "Product 1",
+        "price": 500
+    },
+    {
+        "id": 2,
+        "logo": "https://cdn.albero.com.ua/products/225472ae/b33c/11e7/8931/000c2987a3fe/100x100/3dd06-askania-grande.webp",
+        "title": "Product 2",
+        "price": 600
+    },
+    {
+        "id": 3,
+        "logo": "https://cdn.albero.com.ua/products/225472ae/b33c/11e7/8931/000c2987a3fe/100x100/3dd06-askania-grande.webp",
+        "title": "Product 3",
+        "price": 750
+    }, {
+        "id": 4,
+        "logo": "https://cdn.albero.com.ua/products/225472ae/b33c/11e7/8931/000c2987a3fe/100x100/3dd06-askania-grande.webp",
+        "title": "Product 4",
+        "price": 300
+    }, {
+        "id": 5,
+        "logo": "https://cdn.albero.com.ua/products/225472ae/b33c/11e7/8931/000c2987a3fe/100x100/3dd06-askania-grande.webp",
+        "title": "Product 5",
+        "price": 800
+    },
+];
